16ac6949b26d5b19573cd57a4168ea5e6743df05
Pooh <Jetsadakorn.d@kkumail.com>
2025-02-23 00:47:01 +0700

    add forder cn

---
+++ InitialProject/resources/lang/cn/home.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'ref' => '参考',
+    'references' => '参考资料',
+    'close' => '关闭',
+    'summary' => '摘要',
+    'graphtext' => '报告文章总数 (5年累计)',
+    'year' => '年',
+    'number' => '数量',
+    'before' => '之前',
+];
---
+++ InitialProject/resources/lang/cn/message.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'Home' => '主页',
+    'Researchers' => '研究人员',
+    'ResearchProj' => '研究项目',
+    'ResearchGroup' => '研究团队',
+    'Report' => '报告',
+    'details' => '更多详情',
+    'expertise' => '研究兴趣',
+    'publications' => '近5年内发表的论文',
+    'education' => '教育',
+    'publications2' => '发表论文',
+    'login' => '登录',
+];
