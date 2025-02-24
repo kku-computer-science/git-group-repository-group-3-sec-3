b31404ecda47b4dca916ac100fbdd1e510eb9447
Teeraphat.k <treeraphat.k@kkumail.com>
2025-02-24 19:09:33 +0700

    update cn

---
+++ InitialProject/resources/lang/cn/message.php
@@ -0,0 +1,14 @@
+<?php
+return [
+'Home' => '首页',
+'Researchers' => '研究员',
+'ResearchProj' => '研究项目',
+'ResearchGroup' => '研究小组',
+'Report' => '报告',
+'details' => '详细信息',
+'expertise' => '专业领域',
+'publications' => '出版作品（过去 5 年）',
+'education' => '教育背景',
+'publications2' => '出版作品',
+'login' => 'Login',
+];
